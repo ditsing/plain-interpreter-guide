7fa25df70da878ef9a5973843fb57e4174e73c14
Jing Yang <ditsing@gmail.com>
2018-08-15 15:28:15 +1000

    Add a simple calculator and its makefile.

---
+++ 1-simple-calcuator/calculator.c
@@ -0,0 +1,43 @@
+/**
+ * A simple calculator that computes as it takes input.
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+	printf("Ready for input.\n");
+
+	int c;
+	double first_operand = 0;
+	char last_operator  = '\0';
+	double operand = 0;
+	while ((c = getchar()) != EOF) {
+		char ch = c;
+		if (isdigit(ch)) {
+			operand *= 10;
+			operand += ch - '0';
+		} else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '=') {
+			switch (last_operator) {
+				case '\0':first_operand = operand;
+					  break;
+				case '=': 
+					  break;
+				case '+': first_operand += operand;
+					  break;
+				case '-': first_operand -= operand;
+					  break;
+				case '*': first_operand *= operand;
+					  break;
+				case '/': first_operand /= operand;
+					  break;
+			}
+			if (last_operator != '\0') {
+				printf("=%.2f\n", first_operand);
+			}
+			last_operator = ch;
+			operand = 0;
+		}
+	}
+
+	return 0;
+}
