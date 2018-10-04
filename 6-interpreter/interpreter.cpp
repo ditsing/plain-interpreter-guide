2a261b10c7207c649c708519d043e4b7bab0dbf2
Jing Yang <gitlab@ditsing.com>
2018-10-04 19:46:28 +1000

    Better explanation.

--- 6-interpreter/interpreter.cpp
+++ 6-interpreter/interpreter.cpp
@@ -10,6 +10,9 @@ int main() {
    * Implement the main function. Call `parse_arith_expr` to parse each line as
    * an expression. Evaluate all the expressions and print out the result.
    *
+   * `lexer.h` defines a `lexer` function. Pass that function to
+   * `parse_arith_expr` as parameter.
+   *
    * The program should quit when encountering a compiling error. To make
    * testing easier, do not quit over runtime error. Instead, contiune
    * evaluating subsequence expressions.
